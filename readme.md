925d88cdd19a0045e82e9e0f9783bb5667c874d2
philip <philip.oswald@gmail.com>
2018-01-16 22:49:35 +0100

    Add simple HTTP server

---
+++ readme.md
@@ -0,0 +1,25 @@
+Build:
+
+    $ mvn clean verify
+    $ ./run.sh
+
+Usage:
+
+    $ curl http://localhost:8080/
+    $ curl http://localhost:8080/health
+    $ curl http://localhost:8080/now
+    $ curl http://localhost:8080/req?foo=bar
+    $ curl http://localhost:8080/greet
+    $ curl http://localhost:8080/greet?name=Philip
+    $ curl http://localhost:8080/args
+    $ curl http://localhost:8080/env
+    $ curl http://localhost:8080/env/PATH
+    $ curl http://localhost:8080/prop
+    $ curl http://localhost:8080/prop/os.name
+    $ curl http://localhost:8080/mem
+    $ curl http://localhost:8080/mem/foo
+    $ curl http://localhost:8080/mem/foo?value=bar -X PUT
+    $ curl http://localhost:8080/session
+    $ curl http://localhost:8080/session?logout
+    $ curl http://localhost:8080/log?message=hello -X POST
+    $ curl http://localhost:8080/exit -X POST
---
+++ src/main/java/com/github/phoswald/sample/Application.java
@@ -0,0 +1,108 @@
+package com.github.phoswald.sample;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.log4j.Logger;
+
+import ratpack.handling.Context;
+import ratpack.http.Request;
+import ratpack.server.RatpackServer;
+
+public class Application {
+
+    private static final String SESSION_COOKIE = "MY_SESSION";
+    private static final Logger logger = Logger.getLogger(Application.class);
+    private static final Map<String, String> map = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        logger.info("A simple and cool HTTP server is starting :-)");
+        RatpackServer.start(server -> server
+                .serverConfig(c -> c.port(8080))
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("This is ratpack-demo-service, a simple and cool HTTP server\n"))
+                        .get("health", ctx -> ctx.render("OK\n"))
+                        .get("now", ctx -> ctx.render(ZonedDateTime.now().toString() + "\n"))
+                        .get("req", ctx -> printRequest(ctx))
+                        .get("greet", ctx -> ctx.render("Hello, " + Optional.ofNullable(ctx.getRequest().getQueryParams().get("name")).orElse("Stranger") + "!\n"))
+                        .get("args", ctx -> ctx.render(Arrays.asList(args).toString() + "\n"))
+                        .get("env", ctx -> printMap(ctx, System.getenv()))
+                        .get("env/:name", ctx -> printMapEntry(ctx, System.getenv(), ctx.getPathTokens().get("name")))
+                        .get("prop", ctx -> printMap(ctx, System.getProperties()))
+                        .get("prop/:name", ctx -> printMapEntry(ctx, System.getProperties(), ctx.getPathTokens().get("name")))
+                        .get("mem", ctx -> printMap(ctx, map))
+                        .path("mem/:key", ctx2 -> ctx2.byMethod(chain2 -> chain2
+                                .put(ctx -> storeMapEntry(ctx, map, ctx.getPathTokens().get("key"), ctx.getRequest().getQueryParams().get("value")))
+                                .get(ctx -> printMapEntry(ctx, map, ctx.getPathTokens().get("key")))
+                        ))
+                        .get("session", ctx -> handleSession(ctx, ctx.getRequest().getQueryParams().get("logout") != null))
+                        .post("log", ctx -> { logger.info(Optional.ofNullable(ctx.getRequest().getQueryParams().get("message"))); ctx.render("OK\n"); })
+                        .post("exit", ctx -> System.exit(1))));
+    }
+
+    private static void printRequest(Context ctx) {
+        Request req = ctx.getRequest();
+        StringBuilder sb = new StringBuilder();
+        sb.append(req.getMethod() + " " + req.getPath() + " " + req.getProtocol() + "\n");
+        req.getHeaders().asMultiValueMap()
+                .forEach((key, value) -> sb.append(key + ": " + value + "\n"));
+        sb.append("\n");
+        sb.append("Remote address = " + req.getRemoteAddress() + "\n");
+        sb.append("Local address = " + req.getLocalAddress() + "\n");
+        sb.append("Raw URI = " + req.getRawUri() + "\n");
+        sb.append("URI = " + req.getUri() + "\n");
+        sb.append("Query = " + req.getQuery() + "\n");
+        sb.append("Query Params = " + req.getQueryParams().size() + "\n");
+        req.getQueryParams()
+                .forEach((key, value) -> sb.append(" - " + key + ": " + value + "\n"));
+        sb.append("Cookies = " + req.getCookies().size() + "\n");
+        req.getCookies()
+                .forEach(cookie -> sb.append(" - " + cookie.name() + "=" + cookie.value() + "\n"));
+        ctx.render(sb.toString());
+    }
+
+    private static void printMap(Context ctx, Map<?, ?> map) {
+        StringBuilder sb = new StringBuilder();
+        map.entrySet().stream()
+                .sorted(Comparator.comparing(e -> (String) e.getKey()))
+                .forEach(e -> sb.append(e.getKey() + "=" + e.getValue() + "\n"));
+        if (sb.length() == 0) {
+            sb.append("<empty>\n");
+        }
+        ctx.render(sb.toString());
+    }
+
+    private static void printMapEntry(Context ctx, Map<?, ?> map, String key) {
+        ctx.render(Optional.ofNullable(map.get(key))
+                .map(val -> val.toString())
+                .orElse("<not defined>") + "\n");
+    }
+
+    private static void storeMapEntry(Context ctx, Map<String, String> map, String key, String value) {
+        if (value != null && value.length() > 0) {
+            map.put(key, value);
+        } else {
+            map.remove(key);
+        }
+        ctx.render("OK\n");
+    }
+
+    private static void handleSession(Context ctx, boolean logout) {
+        if (logout) {
+            ctx.getResponse().expireCookie(SESSION_COOKIE);
+            ctx.render("Session: none\n");
+        } else {
+            String cookie = ctx.getRequest().oneCookie(SESSION_COOKIE);
+            if (cookie == null) {
+                cookie = "S" + Instant.now().toEpochMilli();
+                ctx.getResponse().cookie(SESSION_COOKIE, cookie);
+            }
+            ctx.render("Session: " + cookie + "\n");
+        }
+    }
+}
